f16ed3bf2f3235c46e0edeafc1ade969efdde25a
MiellyEllen <maryellen.bowman@gmail.com>
2019-09-27 16:26:35 -0400

    removed findBy

--- explorecali/src/main/java/com/example/ec/repo/TourRepository.java
+++ explorecali/src/main/java/com/example/ec/repo/TourRepository.java
@@ -3,8 +3,6 @@ package com.example.ec.repo;
 import com.example.ec.domain.Tour;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 
 /**
  * Tour Repository Interface
