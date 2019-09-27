7bd0913e77654ebb30b6f100f66967b0e416f6a8
MiellyEllen <maryellen.bowman@gmail.com>
2019-09-27 16:18:25 -0400

    removed findBy

--- explorecali/src/main/java/com/example/ec/repo/TourRepository.java
+++ explorecali/src/main/java/com/example/ec/repo/TourRepository.java
@@ -3,14 +3,11 @@ package com.example.ec.repo;
 import com.example.ec.domain.Tour;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 
 /**
  * Tour Repository Interface
  *
  * Created by Mary Ellen Bowman
  */
-public interface TourRepository extends CrudRepository<Tour, Integer> {
-    List<Tour> findByTourPackageCode(String code);
+public interface TourRepository extends CrudRepository<Tour, Integer>{
 }
