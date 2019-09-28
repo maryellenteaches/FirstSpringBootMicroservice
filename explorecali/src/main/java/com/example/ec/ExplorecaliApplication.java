844cc0e4427be80f2da796a47cb8573d2a9c0e8b
MiellyEllen <maryellen.bowman@gmail.com>
2019-09-28 16:38:22 -0400

    Setup Endingof the video.

--- explorecali/src/main/java/com/example/ec/ExplorecaliApplication.java
+++ explorecali/src/main/java/com/example/ec/ExplorecaliApplication.java
@@ -2,8 +2,13 @@ package com.example.ec;
 
 import com.example.ec.domain.Difficulty;
 import com.example.ec.domain.Region;
+import com.example.ec.service.TourPackageService;
+import com.example.ec.service.TourService;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -15,37 +20,50 @@ import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.ANY;
 import static com.fasterxml.jackson.annotation.PropertyAccessor.FIELD;
 
 @SpringBootApplication
-public class ExplorecaliApplication  {
+public class ExplorecaliApplication implements CommandLineRunner {
+
+    @Value("${ec.importfile}")
+    private String filePath;
+
+    @Autowired
+    private TourPackageService tourPackageService;
+    @Autowired
+    private TourService tourService;
 
     public static void main(String[] args) {
 		SpringApplication.run(ExplorecaliApplication.class, args);
 	}
 
-    /**
+    @Override
+    public void run(String... args) throws Exception {
+        createTourAllPackages();
+        createTours();
+    }
+/**
      * Initialize all the known tour packages
      */
-//	private void createTourAllPackages(){
-//        tourPackageService.createTourPackage("BC", "Backpack Cal");
-//        tourPackageService.createTourPackage("CC", "California Calm");
-//        tourPackageService.createTourPackage("CH", "California Hot springs");
-//        tourPackageService.createTourPackage("CY", "Cycle California");
-//        tourPackageService.createTourPackage("DS", "From Desert to Sea");
-//        tourPackageService.createTourPackage("KC", "Kids California");
-//        tourPackageService.createTourPackage("NW", "Nature Watch");
-//        tourPackageService.createTourPackage("SC", "Snowboard Cali");
-//        tourPackageService.createTourPackage("TC", "Taste of California");
-//    }
+	private void createTourAllPackages(){
+        tourPackageService.createTourPackage("BC", "Backpack Cal");
+        tourPackageService.createTourPackage("CC", "California Calm");
+        tourPackageService.createTourPackage("CH", "California Hot springs");
+        tourPackageService.createTourPackage("CY", "Cycle California");
+        tourPackageService.createTourPackage("DS", "From Desert to Sea");
+        tourPackageService.createTourPackage("KC", "Kids California");
+        tourPackageService.createTourPackage("NW", "Nature Watch");
+        tourPackageService.createTourPackage("SC", "Snowboard Cali");
+        tourPackageService.createTourPackage("TC", "Taste of California");
+    }
 
     /**
      * Create tour entities from an external file
     */
-//    private void createTours() throws IOException {
-//        TourFromFile.read(filePath).forEach(importedTour ->
-//            tourService.createTour(importedTour.getTitle(), importedTour.getDescription(), importedTour.getBlurb(),
-//                    importedTour.getPrice(), importedTour.getLength(), importedTour.getBullets(),
-//                    importedTour.getKeywords(), importedTour.getPackageType(), importedTour.getDifficulty(),
-//                    importedTour.getRegion()));
-//    }
+    private void createTours() throws IOException {
+        TourFromFile.read(filePath).forEach(importedTour ->
+            tourService.createTour(importedTour.getTitle(), importedTour.getDescription(), importedTour.getBlurb(),
+                    importedTour.getPrice(), importedTour.getLength(), importedTour.getBullets(),
+                    importedTour.getKeywords(), importedTour.getPackageType(), importedTour.getDifficulty(),
+                    importedTour.getRegion()));
+    }
 
     /**
      * Helper class to import ExploreCalifornia.json
