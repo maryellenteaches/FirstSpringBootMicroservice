712a733208c8d1e5f44dc7166f02a1fdf228c4ea
MiellyEllen <maryellen.bowman@gmail.com>
2019-09-30 16:21:27 -0400

    Setup for 4-3-begin

--- explorecali/src/main/java/com/example/ec/web/TourRatingController.java
+++ explorecali/src/main/java/com/example/ec/web/TourRatingController.java
@@ -1,10 +1,16 @@
 package com.example.ec.web;
 
+import com.example.ec.domain.Tour;
 import com.example.ec.repo.TourRatingRepository;
 import com.example.ec.repo.TourRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.NoSuchElementException;
 
 /**
  * Tour Rating Controller
@@ -14,8 +20,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/tours/{tourId}/ratings")
 public class TourRatingController {
-    TourRatingRepository tourRatingRepository;
-    TourRepository tourRepository;
+    private TourRatingRepository tourRatingRepository;
+    private TourRepository tourRepository;
 
     @Autowired
     public TourRatingController(TourRatingRepository tourRatingRepository, TourRepository tourRepository) {
@@ -27,5 +33,29 @@ public class TourRatingController {
 
     }
 
+    /**
+     * Verify and return the Tour given a tourId.
+     *
+     * @param tourId tour identifier
+     * @return the found Tour
+     * @throws NoSuchElementException if no Tour found.
+     */
+    private Tour verifyTour(int tourId) throws NoSuchElementException {
+        return tourRepository.findById(tourId).orElseThrow(() ->
+            new NoSuchElementException("Tour does not exist " + tourId));
+        }
+
+    /**
+     * Exception handler if NoSuchElementException is thrown in this Controller
+     *
+     * @param ex exception
+     * @return Error message String.
+     */
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NoSuchElementException.class)
+    public String return400(NoSuchElementException ex) {
+        return ex.getMessage();
+
+    }
 
 }
