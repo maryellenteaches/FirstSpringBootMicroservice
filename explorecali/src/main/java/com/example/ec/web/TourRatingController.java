a2a58c157ca6fb1c2e0664ebeb32f729a2adfc4e
MiellyEllen <maryellen.bowman@gmail.com>
2019-09-30 19:06:29 -0400

    added Get methods

--- explorecali/src/main/java/com/example/ec/web/TourRatingController.java
+++ explorecali/src/main/java/com/example/ec/web/TourRatingController.java
@@ -10,7 +10,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 /**
  * Tour Rating Controller
@@ -46,6 +49,36 @@ public class TourRatingController {
         Tour tour = verifyTour(tourId);
         tourRatingRepository.save(new TourRating( new TourRatingPk(tour, ratingDto.getCustomerId()),
                 ratingDto.getScore(), ratingDto.getComment()));
+    }
+
+
+    /**
+     * Lookup a the Ratings for a tour.
+     *
+     * @param tourId Tour Identifier
+     * @return All Tour Ratings as RatingDto's
+     */
+    @GetMapping
+    public List<RatingDto> getAllRatingsForTour(@PathVariable(value = "tourId") int tourId) {
+        verifyTour(tourId);
+        return tourRatingRepository.findByPkTourId(tourId).stream()
+                .map(RatingDto::new).collect(Collectors.toList());
+    }
+
+    /**
+     * Calculate the average Score of a Tour.
+     *
+     * @param tourId tour identifier
+     * @return Tuple of "average" and the average value.
+     * @throws NoSuchElementException (404) if no ratings exist for the the tour
+     */
+    @GetMapping(path = "/average")
+    public Map<String, Double> getAverage(@PathVariable(value = "tourId") int tourId)
+            throws NoSuchElementException {
+        verifyTour(tourId);
+        return Map.of("average",
+                tourRatingRepository.findByPkTourId(tourId).stream()
+                        .mapToInt(TourRating::getScore).average().getAsDouble());
     }
 
     /**
