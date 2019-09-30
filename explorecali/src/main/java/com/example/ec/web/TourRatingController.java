02e582827a96e69a33d2d1499f6764cc35079f7c
MiellyEllen <maryellen.bowman@gmail.com>
2019-09-30 16:31:11 -0400

    implement post mapping

--- explorecali/src/main/java/com/example/ec/web/TourRatingController.java
+++ explorecali/src/main/java/com/example/ec/web/TourRatingController.java
@@ -1,14 +1,14 @@
 package com.example.ec.web;
 
 import com.example.ec.domain.Tour;
+import com.example.ec.domain.TourRating;
+import com.example.ec.domain.TourRatingPk;
 import com.example.ec.repo.TourRatingRepository;
 import com.example.ec.repo.TourRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.NoSuchElementException;
 
@@ -31,6 +31,21 @@ public class TourRatingController {
 
     protected TourRatingController() {
 
+    }
+
+
+    /**
+     * Create a Tour Rating.
+     *
+     * @param tourId tour identifier
+     * @param ratingDto rating data transfer object
+     */
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createTourRating(@PathVariable(value = "tourId") int tourId, @RequestBody @Validated RatingDto ratingDto) {
+        Tour tour = verifyTour(tourId);
+        tourRatingRepository.save(new TourRating( new TourRatingPk(tour, ratingDto.getCustomerId()),
+                ratingDto.getScore(), ratingDto.getComment()));
     }
 
     /**
