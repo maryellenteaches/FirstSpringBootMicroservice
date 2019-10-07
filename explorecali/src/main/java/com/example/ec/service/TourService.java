e3b98529197f9a976a123b9ffce01f8c3230436d
MiellyEllen <maryellen.bowman@gmail.com>
2019-10-07 13:56:04 -0400

    Stubs of service methods to fill in

---
+++ explorecali/src/main/java/com/example/ec/service/TourService.java
@@ -0,0 +1,55 @@
+package com.example.ec.service;
+
+import com.example.ec.domain.Difficulty;
+import com.example.ec.domain.Region;
+import com.example.ec.domain.Tour;
+import com.example.ec.domain.TourPackage;
+import com.example.ec.repo.TourPackageRepository;
+import com.example.ec.repo.TourRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TourService {
+    private TourRepository tourRepository;
+    private TourPackageRepository tourPackageRepository;
+
+    @Autowired
+    public TourService(TourRepository tourRepository, TourPackageRepository tourPackageRepository) {
+        this.tourRepository = tourRepository;
+        this.tourPackageRepository = tourPackageRepository;
+    }
+
+    /**
+     * Create a new Tour Object and persist it to the Database.
+     *
+     * @param title title
+     * @param description description
+     * @param blurb blurb
+     * @param price price
+     * @param duration duration
+     * @param bullets comma-separated bullets
+     * @param keywords keywords
+     * @param tourPackageName tour package name
+     * @param difficulty difficulty
+     * @param region region
+     * @return Tour Entity
+     */
+    public Tour createTour(String title, String description, String blurb, Integer price,
+                           String duration, String bullets,
+                           String keywords, String tourPackageName, Difficulty difficulty, Region region ) {
+        TourPackage tourPackage = null;
+
+        return new Tour(title, description,blurb, price, duration, bullets, keywords, tourPackage, difficulty, region);
+    }
+
+    /**
+     * Calculate the number of Tours in the Database.
+     *
+     * @return the total.
+     */
+    public long total() {
+        return 0;
+    }
+
+}
